1e97b3203e18ad7c859715082f3ce03608f6876a
Pere Collet Montaña <pcolletredu.tecnocampus.cat>
2020-04-18 18:39:54 +0200

    projectes creats

---
+++ server/src/test/java/com/pcollet/tfg/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcollet.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
